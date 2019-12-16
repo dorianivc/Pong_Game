f4a570467c269511759fa714a2225afb2977a9fc
Dorian Isaac Vallecillo Calderón <dorianivc1@gmail.com>
2019-12-15 19:02:57 -0600

    API CALL Implementation

--- src/juegobola/Bola.java
+++ src/juegobola/Bola.java
@@ -1,8 +1,13 @@
 package juegobola;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.AudioFormat;
@@ -28,13 +33,16 @@ public final class Bola extends Actor {
         public double angulo(int x1, int x2,int y1,int y2){
       return Math.toDegrees( Math.atan(y2-y1/x2-x1));
   }
-    public boolean colision(Model m){
-        int X= (this.x)-max( m.racketa.x,min(this.x, m.racketa.x+ m.racketa.getWeight()));
-        int Y=(this.y)-max( m.racketa.y,min(this.y, m.racketa.y+ m.racketa.getHeight()));
-        if((X*X+Y*Y)<(radio*radio)){
-            
+    public boolean colision(Model m) throws IOException{
+        String parametros="raquetaX="+m.racketa.x+"&"+"raquetaY="+m.racketa.y+"&"+"raquetaH="+m.racketa.getHeight()+"&"+"raquetaW="+m.racketa.getWeight()+"&"+"puntoX="+this.x+"&"+"puntoY="+this.y+"&"+"radio="+this.radio;
+        String APICALL=this.HttpRequestColisiconRaqueta(parametros);
+        //System.out.println("Racket + "+ APICALL );
+        if(Integer.parseInt(APICALL)==0){
+            return false;
+        }else if(Integer.parseInt(APICALL)==1){
+            return true;
         }
-        return (X*X+Y*Y)<(radio*radio);
+        return false;
     }
     void puntaje(Model m){
         for(int veces=0;veces<m.bola.size();veces++){
@@ -58,35 +66,97 @@ public final class Bola extends Actor {
         }
         }
         
+        
     }
-    public boolean colisionCC(Model m){
-        int ax=this.x;
-        int bx=323;
-        int ay=this.y;
-        int by=360;
-        int r1=radio;
-        int r2=m.marco.getRadio();
-        double distancia;
-        distancia = Math.sqrt((ax-bx)*(ax-bx) + (ay-by)*(ay-by));
-        return ( (distancia+2*radio) >= (r1 + r2));
-       }
+    public String HttpRequestColisiconRaqueta(String parameters) throws MalformedURLException, IOException{
+         String LINK="http://localhost:7071/api/ColisiconRacketa";
+         String append=parameters;
+         LINK= LINK+"?"+append;
+         //System.out.println(LINK);
+         String inputLine="empty";
+         try {
+           URL API = new URL(LINK);
+        URLConnection yc = API.openConnection();
+             try (BufferedReader in = new BufferedReader(
+                     new InputStreamReader(
+                             yc.getInputStream()))) {
+                 while ((inputLine = in.readLine()) != null){
+                     //System.out.println(inputLine);
+                     return inputLine;
+                 }
+                 in.close();
+             }
+    }catch(Exception ex){
+             System.out.println(ex.getMessage());
+    }
+        return inputLine;
+         
+     }
+    public String HttpRequestColisiconCC(String parameters) throws MalformedURLException, IOException{
+         String LINK="http://localhost:7071/api/ColisicionCC";
+         String append=parameters;
+         LINK= LINK+"?"+append;
+         //System.out.println(LINK);
+         String inputLine="empty";
+         try {
+           URL API = new URL(LINK);
+        URLConnection yc = API.openConnection();
+             try (BufferedReader in = new BufferedReader(
+                     new InputStreamReader(
+                             yc.getInputStream()))) {
+                 while ((inputLine = in.readLine()) != null){
+                     //System.out.println(inputLine);
+                     return inputLine;
+                 }
+                 in.close();
+             }
+    }catch(Exception ex){
+             System.out.println(ex.getMessage());
+    }
+        return inputLine;
+         
+     }
+    public boolean colisionCC(Model m) throws IOException{
+        Integer ax=this.x;
+        Integer bx=323;
+        Integer ay=this.y;
+        Integer by=360;
+        Integer r1=radio;
+        Integer r2=m.marco.getRadio();
+        String parametros="ax="+ax+"&"+"bx="+bx+"&"+"ay="+ay+"&"+"by="+by+"&"+"r1="+r1+"&"+"r2="+r2;
+        //System.out.println(parametros);
+        String colisionAPICall=this.HttpRequestColisiconCC(parametros);
+       // System.out.println("API CALL Circle-Circle + "+ colisionAPICall);
+        if(Integer.parseInt(colisionAPICall)==1){
+            return true;
+        }else if(Integer.parseInt(colisionAPICall)==0){
+            return false;
+        }
+        return false;
+    }
     @Override
      public void move(Model m){//para configurar el rebote tengo que parase un model por parametro y utilizar los datos de la raqueta
        //rebote con la raqueta
     double puntoCentroX=m.marco.x+m.marco.getRadio();
     double puntoCentroY= m.marco.y+m.marco.getRadio();
-       if(colision(m)){
-           //deltaX=deltaX*-1;
-            deltaY=deltaY*-1;
-       }
-       if(colisionCC(m)){
-          uh.setFramePosition(0);
-          uh.start();
-          uh.setFramePosition(0);
-          puntaje(m);
-           deltaY=deltaY*-1;
-           deltaX=deltaX*-1;
-       }
+        try {
+            if(colision(m)){
+                //deltaX=deltaX*-1;
+                deltaY=deltaY*-1;
+            }} catch (IOException ex) {
+            Logger.getLogger(Bola.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            if(colisionCC(m)){
+                uh.setFramePosition(0);
+                uh.start();
+                uh.setFramePosition(0);
+                puntaje(m);
+                deltaY=deltaY*-1;
+                deltaX=deltaX*-1;
+            }} catch (IOException ex) {
+            Logger.getLogger(Bola.class.getName()).log(Level.SEVERE, null, ex);
+        }
         x=x+deltaX;
         y=y+deltaY;            
      }
--- src/juegobola/JuegoBola.java
+++ src/juegobola/JuegoBola.java
@@ -1,10 +1,19 @@
 package juegobola;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import java.net.*;
+import java.io.*;
 
 
 public class JuegoBola {
